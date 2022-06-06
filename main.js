1e2bbfbc30c5186887b6e014266a0cb2a708557a
evboas <ev.boas@hotmail.com>
2022-06-06 12:48:37 -0300

    Implemantação das funções de botão da seção habilidades

---
+++ main.js
@@ -0,0 +1,32 @@
+const descricaoInicio = document.querySelector('.descricao__inicio');
+
+const topicoEmbarcados = document.querySelector('.btn-embarcados');
+const descricaoEmbarcados = document.querySelector('#embarcados');
+
+const topicoWeb = document.querySelector('.btn-web');
+const descricaoWeb = document.querySelector('#web');
+
+const topicoFront = document.querySelector('.btn-front');
+const descricaoFront = document.querySelector('#front');
+
+const topicoDados = document.querySelector('.btn-dados');
+const descricaoDados = document.querySelector('#dados');
+
+const botoes = [topicoEmbarcados, topicoWeb, topicoFront, topicoDados];
+const descricoes = [descricaoEmbarcados, descricaoWeb, descricaoFront, descricaoDados];
+
+botoes.forEach((botao, i) => {
+    botao.addEventListener('click', () => {
+        console.log(i);
+        descricaoInicio.classList.add('hidden');
+        descricoes.forEach((descricao, j) => {
+            if(!descricao.classList.contains('hidden')) {
+                descricao.classList.add('hidden');
+            };
+            if(i == j){
+                descricao.classList.remove('hidden');
+                console.log(j)
+            };
+        });
+    })
+})
